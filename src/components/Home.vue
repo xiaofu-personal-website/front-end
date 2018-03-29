b7c9f3caffce34320d84ecb7c90f8ab6a4c23a04
xfxfxfxfxf <377625928@qq.com>
2018-03-29 19:11:41 +0800

    添加主页按钮

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,22 @@
     <div class="title">{{ msg }}</div>
     <img class="background-image hidden-xs-only" src="@/../static/home_big.jpg">
     <img class="background-image hidden-sm-and-up" src="@/../static/home_small.jpg">
-    <el-button type="danger" @click="goToHome" plain>主页</el-button>
-    <el-button type="danger" @click="goToAbout" plain>关于作者</el-button>
-    <el-button type="danger" @click="goToArtical" plain>文章</el-button>
-    <el-button type="danger" @click="goToLogIn" plain>作者登陆</el-button>
+    <floating-button class="login-button hidden-xs-only" content="作者登陆" routeName="LogIn" left="80%" top="5%"></floating-button>
+    <floating-button class="artical-button hidden-xs-only" content="文章" routeName="Artical" left="35%" top="20%" width="10em" height="10em" fontSize="1.5em"></floating-button>
+    <floating-button class="artical-button hidden-sm-and-up" content="文章" routeName="Artical" left="40%" top="20%" width="7em" height="7em" fontSize="1.2em"></floating-button>
+    <floating-button class="about-button hidden-xs-only" content="关于作者" routeName="About" left="7%" top="58%" width="7em" height="7em" fontSize="1.2em"></floating-button>
+    <floating-button class="about-button hidden-sm-and-up" content="关于作者" routeName="About" left="15%" top="63%"></floating-button>
   </div>
 </template>
 
 <script>
+import FloatingButton from './components/FloatingButton';
+
 export default {
   name: 'Home',
+  components: {
+    FloatingButton,
+  },
   data () {
     return {
       msg: 'Welcome to 小f的个人网站'
@@ -51,5 +57,37 @@ export default {
   width: 100vw;
   height: 100vh;
   z-index: -1;
+}
+.login-button {
+  animation: aniThree 10s infinite;
+}
+.artical-button {
+  animation: aniTwo 10s infinite;
+}
+.about-button {
+  animation: aniOne 10s infinite;
+}
+@keyframes aniOne {
+  0% {transform: translate(0, 0);}
+  25% {transform: translate(1.5em, .5em);}
+  50% {transform: translate(.5em, -.5em);}
+  75% {transform: translate(1.5em, .5em)}
+  100% {transform: translate(0, 0);}
+}
+
+@keyframes aniTwo {
+  0% {transform: translate(0, 0);}
+  25% {transform: translate(1em, .5em);}
+  50% {transform: translate(.5em, 0);}
+  75% {transform: translate(1em, -.5em)}
+  100% {transform: translate(0, 0);}
+}
+
+@keyframes aniThree {
+  0% {transform: translate(0, 0);}
+  25% {transform: translate(-.5em, 1em);}
+  50% {transform: translate(.5em, 0);}
+  75% {transform: translate(-.5em, .5em)}
+  100% {transform: translate(0, 0);}
 }
 </style>
---
+++ src/components/components/FloatingButton.vue
@@ -0,0 +1,218 @@
+<template>
+  <button
+    class="floating-button"
+    :style="{
+      margin, padding, border, borderRadius, color, backgroundColor,
+      opacity, left, top, width, height, fontSize }"
+    @click="goTo(routeName)">{{ content }}</button>
+</template>
+
+<script>
+export default {
+  name: 'FloatingButton',
+  props: {
+    // 按钮中的内容
+    content: {
+      type: String,
+      default: '',
+    },
+    // 去到哪个页面
+    routeName: {
+      type: String,
+      default: '',
+    },
+    // 外边距
+    margin: {
+      type: String,
+      default: '0',
+    },
+    // 内边距
+    padding: {
+      type: String,
+      default: '0',
+    },
+    // 边框
+    border: {
+      type: String,
+      default: '0',
+    },
+    // 边框弧度
+    borderRadius: {
+      type: String,
+      default: '50%',
+    },
+    // 字体颜色
+    color: {
+      type: String,
+      default: '#fb3',
+    },
+    // 背景颜色
+    backgroundColor: {
+      type: String,
+      default: '#58a',
+    },
+    // 字体颜色
+    opacity: {
+      type: Number,
+      default: 0.8,
+    },
+    // 左边的距离
+    left: {
+      type: String,
+      default: '0',
+    },
+    // 上面的距离
+    top: {
+      type: String,
+      default: '0',
+    },
+    // 宽度
+    width: {
+      type: String,
+      default: '5em',
+    },
+    // 高度
+    height: {
+      type: String,
+      default: '5em',
+    },
+    // 字体大小
+    fontSize: {
+      type: String,
+      default: '1em',
+    },
+  },
+  data() {
+    return {
+      msg: '博客页面',
+    };
+  },
+  methods: {
+    goTo(someWhere) {
+      if (someWhere !== '') {
+        this.$router.push({ name: someWhere });
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.floating-button {
+  display: inline-block;
+  position: absolute;
+  outline: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
+<template>
+  <button
+    class="floating-button"
+    :style="{
+      margin, padding, border, borderRadius, color, backgroundColor,
+      opacity, left, top, width, height, fontSize }"
+    @click="goTo(routeName)">{{ content }}</button>
+</template>
+
+<script>
+export default {
+  name: 'FloatingButton',
+  props: {
+    // 按钮中的内容
+    content: {
+      type: String,
+      default: '',
+    },
+    // 去到哪个页面
+    routeName: {
+      type: String,
+      default: '',
+    },
+    // 外边距
+    margin: {
+      type: String,
+      default: '0',
+    },
+    // 内边距
+    padding: {
+      type: String,
+      default: '0',
+    },
+    // 边框
+    border: {
+      type: String,
+      default: '0',
+    },
+    // 边框弧度
+    borderRadius: {
+      type: String,
+      default: '50%',
+    },
+    // 字体颜色
+    color: {
+      type: String,
+      default: '#fb3',
+    },
+    // 背景颜色
+    backgroundColor: {
+      type: String,
+      default: '#58a',
+    },
+    // 字体颜色
+    opacity: {
+      type: Number,
+      default: 0.8,
+    },
+    // 左边的距离
+    left: {
+      type: String,
+      default: '0',
+    },
+    // 上面的距离
+    top: {
+      type: String,
+      default: '0',
+    },
+    // 宽度
+    width: {
+      type: String,
+      default: '5em',
+    },
+    // 高度
+    height: {
+      type: String,
+      default: '5em',
+    },
+    // 字体大小
+    fontSize: {
+      type: String,
+      default: '1em',
+    },
+  },
+  data() {
+    return {
+      msg: '博客页面',
+    };
+  },
+  methods: {
+    goTo(someWhere) {
+      if (someWhere !== '') {
+        this.$router.push({ name: someWhere });
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.floating-button {
+  display: inline-block;
+  position: absolute;
+  outline: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
